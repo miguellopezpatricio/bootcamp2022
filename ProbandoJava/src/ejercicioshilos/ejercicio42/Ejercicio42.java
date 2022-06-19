d63f13c64f81788dfd9fbff4588e9f4e5c58b4d1
Miguel López Patricio <ml.patri@gmail.com>
2022-06-19 13:56:38 +0200

    Actualizando ejercicios hilos

--- ProbandoJava/src/ejercicioshilos/ejercicio42/Ejercicio42.java
+++ ProbandoJava/src/ejercicioshilos/ejercicio42/Ejercicio42.java
@@ -1,12 +1,9 @@
 package ejercicioshilos.ejercicio42;
 
-import java.awt.Toolkit;
+
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /*
@@ -18,7 +15,7 @@ public class Ejercicio42 {
 
 	public static void main(String[] args) {
 	
-		  Toolkit toolkit = Toolkit.getDefaultToolkit();
+	//	  Toolkit toolkit = Toolkit.getDefaultToolkit();
 
 	        AtomicInteger contador = new AtomicInteger(5);
 
